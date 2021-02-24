6c27d89ccce7566d733ec56b742cbab0154daaec
Jakub Choła <jakub.chola92@gmail.com>
2021-02-24 14:20:31 +0100

    Change build29

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const buildPath = path.join(__dirname, '/../client/', 'build')
 app.use(express.static(buildPath));  
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/build', 'index.html'));
+  res.sendFile(path.join('/../client/build', 'index.html'));
 })
 
 

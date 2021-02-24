effc807657be3db98a80b956fff4e57262040909
Jakub Choła <jakub.chola92@gmail.com>
2021-02-24 08:48:04 +0100

    update mongo

--- server/config.js
+++ server/config.js
@@ -1,4 +1,3 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB: process.env.mongo,
-}
+    DB: 'mongodb+srv://JakubUser:pizda23a@mernv2.vs4zi.mongodb.net/testData?retryWrites=true&w=majority',

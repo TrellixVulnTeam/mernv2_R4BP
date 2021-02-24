13cff299e1e1afa84a2d429068eff9378507e6dc
Jakub Choła <jakub.chola92@gmail.com>
2021-02-24 10:00:24 +0100

    off mongo

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB: 'chuj'
+    DB: 'mongodb+srv://JakubUser:pizda23@@mernv2.vs4zi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }

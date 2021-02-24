852b5756b138502772e33c10242d205c96a65eda
Jakub Choła <jakub.chola92@gmail.com>
2021-02-24 14:08:01 +0100

    Change build25

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    PORT: 3000,
+    PORT: process.env.PORT,
     DB: process.env.DB || 'mongodb+srv://JakubUser:pizda23a@mernv2.vs4zi.mongodb.net/testData?retryWrites=true&w=majority',
 }

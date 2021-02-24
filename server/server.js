229eaa17325f716cdb8b16eb63d5670b1e403fad
Jakub Choła <jakub.chola92@gmail.com>
2021-02-24 12:17:34 +0100

    Change build16

--- server/server.js
+++ server/server.js
@@ -19,10 +19,10 @@ app.use(mongoSanitize())
 
 app.use('/api', postRoutes);
 
-app.use(express.static(path.join(__dirname + '/../client/build')));  
+app.use(express.static(path.join(__dirname,'/../client/build')));  
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '/../client/build/index.html'));
 })
 
 

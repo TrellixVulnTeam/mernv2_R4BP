f1f42167a171546e4695473386eb98c27aa25fd7
Jakub Choła <jakub.chola92@gmail.com>
2021-02-24 08:52:03 +0100

    Add }

--- server/config.js
+++ server/config.js
@@ -1,3 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB: 'mongodb+srv://JakubUser:pizda23a@mernv2.vs4zi.mongodb.net/testData?retryWrites=true&w=majority',
+    DB: 'mongodb+srv://JakubUser:pizda23a@mernv2.vs4zi.mongodb.net/testData?retryWrites=true&w=majority'
+}

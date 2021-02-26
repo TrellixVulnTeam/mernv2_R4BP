a007a0217a76646333c3f51fef71af63c79a3822
Jakub Choła <jakub.chola92@gmail.com>
2021-02-26 15:05:26 +0100

    Add mern string

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    DB: process.env.DB,
+    DB: process.env.DB || 'mongodb+srvJakubUserpizda23a@mernv2.vs4zi.mongodb.nettestDataretryWrites=true&w=majority'
 }

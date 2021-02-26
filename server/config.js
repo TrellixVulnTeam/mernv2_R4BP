6e1da3569b53c271b2e379f7c3446422a3e851a0
Jakub Choła <jakub.chola92@gmail.com>
2021-02-26 14:55:40 +0100

    Back to branch master

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    PORT: process.env.PORT || 3000,
-    DB: process.env.DB || 'mongodb+srv://JakubUser:pizda23a@mernv2.vs4zi.mongodb.net/testData?retryWrites=true&w=majority'
+    PORT: process.env.PORT || 8000,
+    DB: process.env.DB,
 }
